b4fe6cf6ba6370ef53b2e059e5f14c451a490f86
Jones Sabino <jones.sabino@gmail.com>
2024-06-27 15:59:08 -0300

    adjusted web navigation

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class Config:
     BASE_URL = "https://www.latimes.com/"
     OUTPUT_DIR = "output"
     EXCEL_FILE = os.path.join(OUTPUT_DIR, "news_data.xlsx")
+    NEWEST='1'
 
     # Locators
     LOCATORS = Box(
--- src/rpa_news_scraper.py
+++ src/rpa_news_scraper.py
@@ -1,13 +1,16 @@
+import contextlib
 from RPA.Browser.Selenium import Selenium
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from loguru import logger
 from src.config import Config
 import traceback
 from box import Box
 from selenium.webdriver.chrome.options import Options
+
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 
 class RPANewsScraper:
@@ -17,7 +20,9 @@ class RPANewsScraper:
         self.topic = topic
         self.months = months
         self.articles = []
-        self.browser = Selenium()
+        self.browser = Selenium(
+            timeout=10, implicit_wait=10, run_on_failure="CapturePageScreenshot"
+        )
         self.locators = Box(
             {
                 "search_field_button": 'css:button[data-element="search-button"]',
@@ -25,14 +30,25 @@ class RPANewsScraper:
                 "submit_button": 'css:button[data-element="search-submit-button"]',
                 "sort_by_button": "css:select.select-input",
                 "filter": "css:div.search-results-module-filters-title",
+                "see_all_topics": "xpath://ul[@data-name='Topics']/ancestor::ps-toggler//span[@class='see-all-text']",
+                "topic": f"xpath://label[span[text()='{self.topic}']]/input",
+                "topic_selected_count": f"//span[text()='{self.topic}']/../../following-sibling::div[contains(@class, 'SearchFilterInput-count')]",
+                "search_results_count": "xpath://span[contains(text(), 'There are <?> results that match your search.')]",
+                "search_results": "css:ul.search-results-module-results-menu li",
+                "article_title": "css:h3.promo-title > a",
+                "article_description": "css:p.promo-description",
+                "article_date": "css:p.promo-timestamp",  # attr data-timestamp
+                "article_image": "css:img.image",
+                "next_page": "css:div.search-results-module-next-page",
+                "loading_icon": "css:div.loading-icon",
             }
         )
 
-    def create_webdriver_options(self):
+    def _create_webdriver_options(self):
         options = Options()
         options.add_argument("--enable-automation")
         options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--window-size=1024,768")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-extensions")
         options.add_argument("--dns-prefetch-disable")
@@ -41,12 +57,11 @@ class RPANewsScraper:
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--ignore-ssl-errors")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-software-rasterizer")
         options.add_argument("--log-level=3")
         options.add_argument("enable-features=NetworkServiceInProcess")
         options.add_argument("disable-features=NetworkService")
-        options.set_capability("pageLoadStrategy", "normal")
-
+        options.set_capability("pageLoadStrategy", "eager")
         return {
             "arguments": options.arguments,
             "capabilities": options.to_capabilities(),
@@ -55,63 +70,101 @@ class RPANewsScraper:
     def open_site(self):
         logger.info(f"Opening site {self.base_url}")
         try:
-            options = self.create_webdriver_options()
+            options = self._create_webdriver_options()
             self.browser.open_available_browser(self.base_url, options=options)
         except Exception:
-            self._finish_process('Failed to open site: ', traceback.format_exc())
+            self._finish_process("Failed to open site: ", traceback.format_exc())
 
-    def search(self):
+    def _search(self):
         try:
             logger.info(f"Searching for '{self.search_phrase}'")
-            self.browser.wait_until_page_contains_element(self.locators.search_field_button, timeout=300)
-            self.browser.click_button(self.locators.search_field_button)
-            self.browser.input_text(locator=self.locators.search_field, text=self.search_phrase)
-            self.browser.click_button(self.locators.submit_button) #type: ignore
-            self.browser.wait_until_page_contains(self.locators.filter)
+            self.browser.wait_and_click_button(self.locators.search_field_button)
+            self.browser.input_text(
+                locator=self.locators.search_field, text=self.search_phrase
+            )
+            self.browser.click_button(self.locators.submit_button)
+
+            self.browser.wait_until_element_is_visible(self.locators.see_all_topics)
+            self.browser.click_element(self.locators.see_all_topics)
+
         except Exception:
-            self._finish_process('Search failed: ', traceback.format_exc())
+            self._finish_process("Search failed: ", traceback.format_exc())
 
-    def select_topic(self):
-        if self.topic:
-            try:
-                logger.info(f"Selecting topic '{self.topic}'")
-                self.browser.click_link(self.topic)  #type: ignore
-                self.browser.wait_until_page_contains(self.topic)
-                self.locators
-            except Exception:
-                self._finish_process('Category selection failed: ', traceback.format_exc())
-
-    def parse_articles(self):
+    def _select_topic(self):
         try:
-            logger.info("Parsing articles")
-            articles = self.browser.find_elements(Config.LOCATORS["article"])   #type: ignore
-            for article in articles:
-                title = self.browser.find_element(
-                    Config.LOCATORS["article_title"], article
-                ).text
-                description = self.browser.find_element(
-                    Config.LOCATORS["article_description"], article
-                ).text
-                date_text = self.browser.find_element(
-                    Config.LOCATORS["article_date"], article
-                ).get_attribute("datetime")
-                date = datetime.strptime(date_text, "%Y-%m-%dT%H:%M:%SZ")
+            self.browser.wait_until_element_is_visible(self.locators.topic)
+            total_articles_number = self.browser.get_text(
+                self.locators.topic_selected_count
+            )[1:-1]  # type: ignore
+            search_results_count = self.locators.search_results_count.replace(
+                "<?>", total_articles_number
+            )
+            self.browser.select_checkbox(self.locators.topic)
+
+            self.browser.wait_until_element_is_visible(search_results_count)  # type: ignore
+            self.browser.select_from_list_by_value(
+                self.locators.sort_by_button, Config.NEWEST
+            )
+
+            with contextlib.suppress(AssertionError):
+                self.browser.wait_until_element_is_visible(
+                    self.locators.loading_icon, timeout=5
+                )
+                self.browser.wait_until_element_is_not_visible(
+                    self.locators.loading_icon, timeout=3
+                )
+        except Exception:
+            self._finish_process("Selecting topic failed: ", traceback.format_exc())
+
+    def _parse_articles(self):
+        data = {
+            "Title": "",
+            "Date": "",
+            "Description": "",
+            "Picture Filename": "",
+            "Search Phrase Count": "",
+            "Has Money": bool,
+        }
+        try:
+            search_results = self.browser.get_webelements(self.locators.search_results)
+            for article in search_results:
+                logger.info(f"Article {article}")
+                title_element = self.browser.find_element(
+                    self.locators.article_title, article
+                )
+                description_element = self.browser.find_element(
+                    self.locators.article_description, article
+                )
+                date_element = self.browser.find_element(
+                    self.locators.article_date, article
+                )
+
+                title = self.browser.get_text(title_element)
+                description = self.browser.get_text(description_element)
+                date_timestamp = self.browser.get_element_attribute(
+                    date_element, "data-timestamp"
+                )
+                
+                date = datetime.fromtimestamp(
+                    int(date_timestamp) / 1000, timezone.utc
+                ).strftime("%d-%m-%Y")
 
                 if self.is_within_date_range(date):
-                    image_url = self.browser.find_element(
-                        Config.LOCATORS["article_image"], article
-                    ).get_attribute("src")
-                    self.articles.append(
-                        {
-                            "title": title,
-                            "description": description,
-                            "date": date,
-                            "image_url": image_url,
-                        }
-                    )
+                    image_url = self.browser.find_element(Config.LOCATORS['article_image'], article).get_attribute('src')
+                    self.articles.append({
+                        'title': title,
+                        'description': description,
+                        'date': date,
+                        'image_url': image_url,
+                    })
                     logger.info(f"Article found: {title}")
+
+                logger.info(f"Article found: {title}")
+                logger.info(f"Description: {description}")
+                logger.info(f"Date_timestamp: {date_timestamp}")
+                logger.info(f"Date: {date}")
         except Exception:
-            self._finish_process('Parsing articles failed: ', traceback.format_exc())
+            self._finish_process("Parsing articles failed: ", traceback.format_exc())
 
     def _finish_process(self, message, exception):
         logger.error(f"{message}\n\n{exception}")
@@ -129,7 +182,7 @@ class RPANewsScraper:
 
     def extract_data(self):
         self.open_site()
-        self.search()
-        # self.select_topic()
-        # self.parse_articles()
+        self._search()
+        self._select_topic()
+        self._parse_articles()
         # self.close()
